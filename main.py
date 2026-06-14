ff06336d049c46b06aa247a2cacd72fb2ccff31b
pranavmohantyy <mohantypranav147@gmail.com>
2026-06-14 16:24:30 -0700

    implement cli commands for todo

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import argparse
 
 @dataclass
 class Todo:
@@ -12,18 +13,50 @@ def add_todo(title):
     new_id = len(todos) + 1
     todos.append(Todo(id=new_id, title=title))
 
+
 def remove_todo(todo_id):
     global todos
     todos = [todo for todo in todos if todo.id != todo_id]
 
-def print_todos():
+
+def list_todos():
     for todo in todos:
         status = '✓' if todo.done else '✗'
         print(f'{todo.id}: {todo.title} [{status}]')
 
-# Example usage
-add_todo('Learn Python')
-add_todo('Build a CLI')
-print_todos()
-remove_todo(1)
-print_todos()
+
+def mark_done(todo_id):
+    for todo in todos:
+        if todo.id == todo_id:
+            todo.done = True
+            break
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Todo CLI manager')
+    subparsers = parser.add_subparsers(dest='command')
+
+    add_parser = subparsers.add_parser('add')
+    add_parser.add_argument('title', type=str, help='Title of the todo')
+
+    done_parser = subparsers.add_parser('done')
+    done_parser.add_argument('id', type=int, help='ID of the todo to mark as done')
+
+    remove_parser = subparsers.add_parser('remove')
+    remove_parser.add_argument('id', type=int, help='ID of the todo to remove')
+
+    list_parser = subparsers.add_parser('list', help='List all todos')
+
+    args = parser.parse_args()
+
+    if args.command == 'add':
+        add_todo(args.title)
+    elif args.command == 'done':
+        mark_done(args.id)
+    elif args.command == 'remove':
+        remove_todo(args.id)
+    elif args.command == 'list':
+        list_todos()
+
+if __name__ == '__main__':
+    main()
