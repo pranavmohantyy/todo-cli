52e212cc9929e8205ae1a8eb27aa375d8d21b043
pranavmohantyy <mohantypranav147@gmail.com>
2026-06-14 20:04:53 -0700

    add json persistence

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from dataclasses import dataclass
 import argparse
+import json
+import os
 
-@dataclass
+dataclass
 class Todo:
     id: int
     title: str
@@ -9,14 +11,19 @@ class Todo:
 
 todos = []
 
+TODO_FILE = os.path.expanduser('~/.todos.json')
+
+
 def add_todo(title):
     new_id = len(todos) + 1
     todos.append(Todo(id=new_id, title=title))
+    save_todos()
 
 
 def remove_todo(todo_id):
     global todos
     todos = [todo for todo in todos if todo.id != todo_id]
+    save_todos()
 
 
 def list_todos():
@@ -30,33 +37,24 @@ def mark_done(todo_id):
         if todo.id == todo_id:
             todo.done = True
             break
+    save_todos()
+
+
+def save_todos():
+    with open(TODO_FILE, 'w') as f:
+        json.dump([todo.__dict__ for todo in todos], f)
+
+
+def load_todos():
+    global todos
+    if os.path.exists(TODO_FILE):
+        with open(TODO_FILE, 'r') as f:
+            todos = [Todo(**data) for data in json.load(f)]
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Todo CLI manager')
-    subparsers = parser.add_subparsers(dest='command')
-
-    add_parser = subparsers.add_parser('add')
-    add_parser.add_argument('title', type=str, help='Title of the todo')
-
-    done_parser = subparsers.add_parser('done')
-    done_parser.add_argument('id', type=int, help='ID of the todo to mark as done')
-
-    remove_parser = subparsers.add_parser('remove')
-    remove_parser.add_argument('id', type=int, help='ID of the todo to remove')
-
-    list_parser = subparsers.add_parser('list', help='List all todos')
-
-    args = parser.parse_args()
-
-    if args.command == 'add':
-        add_todo(args.title)
-    elif args.command == 'done':
-        mark_done(args.id)
-    elif args.command == 'remove':
-        remove_todo(args.id)
-    elif args.command == 'list':
-        list_todos()
+    load_todos()
+    # add argparse functionality here
 
 if __name__ == '__main__':
     main()
